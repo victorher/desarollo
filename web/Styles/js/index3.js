502cd60af844250f2c79ba7e7c50c9c2f295cc75
victorher <61486282+victorher@users.noreply.github.com>
2020-06-04 18:24:18 -0500

    Update index3.js
    Aporte de Java Script a la ventana principal de entre sueños

--- web/Styles/js/index3.js
+++ web/Styles/js/index3.js
@@ -11,4 +11,25 @@ $(document).ready(function(e){
         }
         ubicacionPrincipal = desplazamientoActual;
     });
+    
+    var btnAbrirPopup = document.getElementById('btnAbrirPopup');
+    var overley = document.getElementById('overley');
+    var popup = document.getElementById('popup');
+    var btnCerrarPopup = document.getElementById('btnCerrar');
+    var btnFormularioIniciar = document.getElementById('iniciar');
+    var btnFormularioRegistrar = document.getElementById('registrar');
+    var FormularioIniciar = document.getElementById('FormularioIniciar');
+    var formularioRegistar = document.getElementById('formularioRegistar');
+    
+    btnAbrirPopup.addEventListener('click', function(){
+        overley.classList.add('active');
+        popup.classList.add('active');
+    });
+    btnCerrarPopup.addEventListener('click', function(){
+        overley.classList.remove('active');
+        popup.classList.remove('active');
+    });
+    btnFormularioIniciar.addEventListener('click', function() {
+        FormularioIniciar.classList.add('active');
+    });
 });
