da1768d8424d65cc878fb8a271f844bcaef59428
VICTORH <VICTORH@192.168.0.11>
2020-06-03 20:40:04 -0500

    Se incluye nueva variabla para llevar los datos del select

--- web/Vista/js/pqrLlevaDatos.js
+++ web/Vista/js/pqrLlevaDatos.js
@@ -3,16 +3,18 @@ $(document).ready(function (){
 
     $('.btn-cargaDato').on('click', function (){
 //        alert('ingresa');
+        var pqr = $('.pqr').val();
         var nom = $('.nombre').val();
         var ema = $('.email').val();
         var mens = $('.mensaje').val();
         
-//        alert(nom+" "+ema+" "+mens);
+//        alert(pqr+" "+nom+" "+ema+" "+mens);
 
         $.ajax({
             type: 'POST',
             url: "ServletCargaPQR",
             data:{
+                tipo: pqr,
                 nombre: nom,
                 email: ema,
                 mensaje: mens
