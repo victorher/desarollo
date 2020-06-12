2a20d845f015d54bf51c66889ecc650c4c9d5550
victorher <61486282+victorher@users.noreply.github.com>
2020-06-12 16:39:51 -0500

    Update modificar.js
    Cambio en el método en que llevamos los datos Ajax -> Old, Fetch -> New

--- web/Styles/js/modificar.js
+++ web/Styles/js/modificar.js
@@ -1,47 +1,76 @@
 $(document).ready(function(){
     //alert("hola");
     
-    $('.btnmod').on('click',function(){
-       var btn = $('.btnmod').index(this);
-        //alert(btn);
-        //asignacion de nombres
-       var iddis = $('.iddis').eq(btn);
-       var nudeta = $('.nudeta').eq(btn);
-       var tedis = $('.tedis').eq(btn);
-       var tadis = $('.tadis').eq(btn);
-       var codis = $('.codis').eq(btn);
-       var predis = $('.predis').eq(btn);
-       var obdis = $('.obdis').eq(btn);
-       var imgdis = $('.imgdis').eq(btn);
-       //declarar variables
-       var  id = iddis.val();
-       var  num = nudeta.val();
-       var  ted = tedis.val();
-       var  tad = tadis.val();
-       var  col = codis.val();
-       var  pre = predis.val();
-       var  obs = obdis.val();
-       var  img = imgdis.val();
-       //mostrar los datos
-        //alert(id+"\n"+num+"\n"+ted+"\n"+tad+"\n"+col+"\n"+pre+"\n"+obs+"\n"+img);
-       $.ajax({
-           type: 'POST',
-           url:'ServletDisenoActualiza',
-           data:{ //es un arreglo
-               iddiseno: id,
-               numdetalle: num,
-               texdiseno: ted,
-               tamdiseno: tad,
-               coldiseno: col,
-               prediseno: pre,
-               obsdiseno: obs,
-               imgdiseno: img
-           },
-            success: function (res) {
-                console.log(res);
+//    $('.btnmod').on('click',function(){
+//       var btn = $('.btnmod').index(this);
+//        //alert(btn);
+//        //asignacion de nombres
+//       var iddis = $('.iddis').eq(btn);
+//       var nudeta = $('.nudeta').eq(btn);
+//       var tedis = $('.tedis').eq(btn);
+//       var tadis = $('.tadis').eq(btn);
+//       var codis = $('.codis').eq(btn);
+//       var predis = $('.predis').eq(btn);
+//       var obdis = $('.obdis').eq(btn);
+//       var imgdis = $('.imgdis').eq(btn);
+//       //declarar variables
+//       var  id = iddis.val();
+//       var  num = nudeta.val();
+//       var  ted = tedis.val();
+//       var  tad = tadis.val();
+//       var  col = codis.val();
+//       var  pre = predis.val();
+//       var  obs = obdis.val();
+//       var  img = imgdis.val();
+//       //mostrar los datos
+//        //alert(id+"\n"+num+"\n"+ted+"\n"+tad+"\n"+col+"\n"+pre+"\n"+obs+"\n"+img);
+//       $.ajax({
+//           type: 'POST',
+//           url:'ServletDisenoActualiza',
+//           data:{ //es un arreglo
+//               iddiseno: id,
+//               numdetalle: num,
+//               texdiseno: ted,
+//               tamdiseno: tad,
+//               coldiseno: col,
+//               prediseno: pre,
+//               obsdiseno: obs,
+//               imgdiseno: img
+//           },
+//            success: function (res) {
+//                console.log(res);
+//            }
+//       });
+//       
+//    });
+
+   $('.btnmod').on('click',function(e){
+        e.preventDefault();
+        var a = $('.btnmod').index(this);
+        console.log(a);
+        var data = new FormData(document.getElementById('frm'));
+        data.append('op', 'envia');
+//        console.log(data.get('iddis'));
+//        console.log(data.get('numdetalle'));
+//        console.log(data.get('imgdiseno'));
+        fetch('ServletDisenoActualiza',
+        {
+            method: "POST",
+            body: data
+        })
+        .then(datos=>datos.json())
+        .then(datos=>{
+            console.log(datos);
+            if(datos){
+                alert('La imagen se cargo correctamente');
             }
-       });
-       
+            else{
+                alert('Los datos no se cargaron');
+            }
+        })
+        .catch (function() {
+            alert('No hay datos');
+        });
     });
 });
 
