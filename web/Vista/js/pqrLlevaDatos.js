078159c5b587310f2fc613ee0f15a4769ec3de2b
VICTORH <VICTORH@192.168.0.11>
2020-06-12 23:16:21 -0500

    Ajuestes en las variables para llevar los datos del registro de una pqr

--- web/Vista/js/pqrLlevaDatos.js
+++ web/Vista/js/pqrLlevaDatos.js
@@ -3,12 +3,12 @@ $(document).ready(function (){
 
     $('.btn-cargaDato').on('click', function (){
 //        alert('ingresa');
-        var pqr = $('.pqr').val();
+        var pqr = $('.tipo').val();
         var nom = $('.nombre').val();
         var ema = $('.email').val();
         var mens = $('.mensaje').val();
         
-//        alert(pqr+"\n"+nom+"\n"+ema+"\n"+mens);
+//        console.log(pqr+"\n"+nom+"\n"+ema+"\n"+mens);
 
         $.ajax({
             type: 'POST',
