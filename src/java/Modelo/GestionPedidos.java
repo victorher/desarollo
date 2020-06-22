0eb277eb6a31e3fd7fd279f1d10c9cc2967cf054
AnderBeltran <64803465+AnderBeltran@users.noreply.github.com>
2020-06-22 00:59:27 -0500

    Update GestionPedidos.java
    Se cambiaron posiciones según la consulta

--- src/java/Modelo/GestionPedidos.java
+++ src/java/Modelo/GestionPedidos.java
@@ -109,15 +109,14 @@ public class GestionPedidos {
            
             while(rs.next()){
                 PedidoUnogetset consemp = new PedidoUnogetset(
-                        rs.getString(1), 
-                        rs.getString(2), 
                         rs.getString(3), 
                         rs.getString(4), 
-                        rs.getString(5), 
-                        rs.getString(6), 
-                        rs.getString(7), 
-                        rs.getInt(8), 
-                        rs.getString(9));
+                        rs.getString(2), 
+                        rs.getString(5),  
+                        rs.getString(9), 
+                        rs.getString(10),
+                        rs.getString(11));
+                
                 lista.add(consemp);
             }
         } catch (Exception e) {
