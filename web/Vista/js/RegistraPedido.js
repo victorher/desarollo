1c36004044226128b6adb264bad67c9fd9f078b8
victorher <61486282+victorher@users.noreply.github.com>
2020-06-21 23:50:11 -0500

    Update RegistraPedido.js
    Ajuste de variebles

--- web/Vista/js/RegistraPedido.js
+++ web/Vista/js/RegistraPedido.js
@@ -2,17 +2,18 @@ $(document).ready(function (){
 //    alert('Hola');
 
     $('.btnEnviar').on('click', function (){
-        var doc = $('.documento').val();
-        var est = $('.estado').val();
+        
+        var doc = $(this).parent().find('.tdocumento').val();
+        var est = $(this).parent().find('.testado').val();
 //        var fIni = $('.dateini').val();
 //        var fFin = $('.datefin').val();
-        var dir = $('.direccion').val();
-        var tip = $('.tipo').val();
-        var art = $('.articulo').val();
-        var dis = $('.diseno').val();
-        var des = $('.descripcion').val();
-        var can = $('.cantidad').val();
-        var pre = $('.precio').val();
+        var dir = $(this).parent().find('.tdireccion').val();
+        var tip = $(this).parent().find('.ttipo').val();
+        var art = $(this).parent().find('.tarticulo').val();
+        var dis = $(this).parent().find('.tdiseno').val();
+        var des = $(this).parent().find('.tdescripcion').val();
+        var can = $(this).parent().find('.tcantidad').val();
+        var pre = $(this).parent().find('.tprecio').val();
         
 //        alert("Documento: "+doc+"\n Estado: "+est+"\n Direccion: "+dir+"\n Tipo: "+tip+"\n IDArticulo: "+art+"\n IDDiseno: "+dis+"\n Descripcion: "+des+"\n Cantidad: "+can+"\n Precio: "+pre);
         
