a216282e439aa9fbb7ae89f1dade10ee31e78cf6
VICTORH <VICTORH@FINCAJARPDH>
2020-06-22 20:33:53 -0500

    Ajuste de variables

---
+++ src/java/Controlador/ServletActualizaEstadoPedido.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.GestionPedidos;
+import Modelo.PedidoUnogetset;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author VICTORH
+ */
+@WebServlet(name = "ServletActualizaEstadoPedido", urlPatterns = {"/ServletActualizaEstadoPedido"})
+public class ServletActualizaEstadoPedido extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            String Cod, Estado;
+            Cod = request.getParameter("cod");
+            Estado = request.getParameter("estado");
+//            Estado = "Entregado";
+            
+//            JOptionPane.showMessageDialog(null, Cod+" "+Estado);
+            
+            PedidoUnogetset ped = new PedidoUnogetset(Cod, Estado);
+            GestionPedidos ge = new GestionPedidos();
+            boolean dat = ge.ActaulizarPedidosNuevos(ped);
+            if(dat){
+                out.println("Datos guardados");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
